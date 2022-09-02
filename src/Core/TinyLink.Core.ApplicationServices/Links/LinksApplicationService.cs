df9fe7669b47ef68abb51e0a668689e363dac8a3
MOHSEN\Mohsen <ilkhany.mohsen@gmail.com>
2022-09-02 11:20:16 +0430

    impelement Links contracts on application services

---
+++ src/Core/TinyLink.Core.ApplicationServices/Links/LinksApplicationService.cs
@@ -0,0 +1,43 @@
+﻿using TinyLink.Common.Core.ApplicationServices;
+using TinyLink.Core.Domain.Links.Contracts;
+using TinyLink.Core.Domain.Links.Entities;
+using TinyLink.Core.Domain.Links.Models;
+
+namespace TinyLink.Core.ApplicationServices.Links
+{
+    public class LinksApplicationService : ApplicationService<LinksApplicationService>, ILinksApplicationService
+    {
+        private readonly ILinksRepository linksRepository;
+        private readonly IUrlHasher urlHasher;
+
+        public LinksApplicationService(ApplicationServiceContext context, ILinksRepository linksRepository, IUrlHasher urlHasher) : base(context)
+        {
+            this.linksRepository = linksRepository;
+            this.urlHasher = urlHasher;
+        }
+
+        public async Task<ApplicationServiceResult<CreateLinkResponse>> CreateLink(CreateLinkRequest request)
+        {
+            return await Execute(request, CreateLinkHandler);
+        }
+
+        private async Task<CreateLinkResponse> CreateLinkHandler(CreateLinkRequest request) 
+        {
+            var link = new Link()
+            {
+                Url = request.Url,
+                CreatedAt = DateTime.Now,
+                IsVisible = true,
+                TotalVistCount = 0
+            };
+            link.Code = await urlHasher.Hash(request.Url);
+
+            await linksRepository.AddLink(link);
+
+            return new CreateLinkResponse()
+            {
+                Code = link.Code
+            };
+        }
+    }
+}
---
+++ src/Core/TinyLink.Core.ApplicationServices/Links/UrlHasherGuid.cs
@@ -0,0 +1,12 @@
+﻿using TinyLink.Core.Domain.Links.Contracts;
+
+namespace TinyLink.Core.ApplicationServices.Links
+{
+    public class UrlHasherGuid : IUrlHasher
+    {
+        public Task<string> Hash(string url)
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+    }
+}
