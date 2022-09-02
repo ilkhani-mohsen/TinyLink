ffb8d4bbe5b8a6748580bdb6100b14e686f926be
MOHSEN\Mohsen <ilkhany.mohsen@gmail.com>
2022-09-02 11:46:48 +0430

    add common classes for persistence layer and dbset of link entity to dbcontext

---
+++ src/Infrastructures/TinyLink.Persistence.SqlServer/Links/TypeConfigurations/LinkTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TinyLink.Core.Domain.Links.Entities;
+
+namespace TinyLink.Persistence.SqlServer.Links.TypeConfigurations
+{
+    public class LinkTypeConfiguration : IEntityTypeConfiguration<Link>
+    {
+        private const string TableName = "Links";
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.ToTable(TableName);
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Url)
+                .IsRequired()
+                .HasMaxLength(2048);
+
+            builder.Property(x => x.Code)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.HasIndex(x => x.Code)
+                .IsUnique();
+        }
+    }
+}
---
+++ src/Infrastructures/TinyLink.Persistence.SqlServer/TinyLinkDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TinyLink.Core.Domain.Links.Entities;
+
+namespace TinyLink.Persistence.SqlServer
+{
+    public class TinyLinkDbContext : DbContext
+    {
+        public DbSet<Link> Links { get; set; }
+
+        public TinyLinkDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
