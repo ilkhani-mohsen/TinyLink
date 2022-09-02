ee8a4a116244cd2bf721c3a0d0acef30d91a10fc
MOHSEN\Mohsen <ilkhany.mohsen@gmail.com>
2022-09-02 11:50:33 +0430

    impelement ILinksRepository on persistence layer

---
+++ src/Infrastructures/TinyLink.Persistence.SqlServer/Links/LinksRepository.cs
@@ -0,0 +1,20 @@
+﻿using TinyLink.Core.Domain.Links.Contracts;
+using TinyLink.Core.Domain.Links.Entities;
+
+namespace TinyLink.Persistence.SqlServer.Links
+{
+    public class LinksRepository : ILinksRepository
+    {
+        private readonly TinyLinkDbContext dbContext;
+
+        public LinksRepository(TinyLinkDbContext tinyLinkDbContext)
+        {
+            dbContext = tinyLinkDbContext;
+        }
+        public async Task AddLink(Link link)
+        {
+            await dbContext.Links.AddAsync(link);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
